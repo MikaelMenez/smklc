39da9410b19a409c5d01419b2a42943904f2545d
Mikael Menezes <mixaelmenezes@gmail.com>
2026-07-30 23:14:10 -0300

    rewrite but right this time starting as getting attrs

--- src/main.rs
+++ src/main.rs
@@ -12,72 +12,29 @@ use std::fs;
 
 
  */
-fn get_tag(txt: &[char]) -> Vec<Tags> {
-    let mut ptr1: usize;
-    let mut ptr2: usize;
-    let mut tags: Vec<Tags> = vec![];
-    let mut i = 0;
-    let mut atributs: bool = false;
-    let mut attrptr: usize;
-    while i < txt.len() {
-        let mut actual = txt[i];
-        if actual == '|' {
-            ptr1 = i;
-            i += 1;
-            actual = txt[i];
-            while actual != '|' {
-                //mexer aqui para ele reconhecer atributos
-                if actual == '(' {
-                    atributs = true;
-                    attrptr = i;
-                    let mut atributtes: Vec<Atributte> = vec![];
-                    i += 1;
-                    actual = txt[i];
-                    let mut tempptr1 = i;
-                    let mut tempptr2 = i;
-                    while actual != ')' {
-                        if actual == '=' {
-                            let name = txt[tempptr1..tempptr2].iter().collect::<String>();
-                            i += 1;
-                            actual = txt[i];
-                            let mut tempptr1: usize;
-                            let mut tempptr2 = i;
-                            if actual == '\"' {
-                                i += 1;
-                                actual = txt[i];
-                                tempptr1 = i;
-                                while actual != '\"' {
-                                    i += 1;
-                                    actual = txt[i];
-                                    tempptr2 += 1;
-                                }
-                                let content =
-                                    txt[tempptr1 + 1..tempptr2].iter().collect::<String>();
-                                atributtes.push(Atributte { name, content });
-                            };
-                        }
-                        i += 1;
-                        actual = txt[i];
-                        tempptr2 = i;
-                    }
-                }
-                if actual == '\0' {
-                    panic!("non finished void tag");
-                }
-                i += 1;
-                actual = txt[i];
-            }
-
-            ptr2 = i;
-
-            tags.push(Tags::VoidTag(VoidTag::new(
-                txt[ptr1 + 1..ptr2].iter().collect::<String>(),
-                Vec::with_capacity(1),
-            )));
-        }
+fn get_attrs(text: &[char]) -> Vec<Atributte> {
+    let mut attrs = vec![];
+    let mut ptr: usize = 0;
+    let mut offset: usize = 0;
+    let mut i: usize = 0;
+    while text[i] != '=' {
         i += 1;
     }
-    tags
+    offset = i - offset;
+    let name: String = text[ptr..ptr + offset].iter().collect();
+
+    offset = 0;
+    i += 2;
+    ptr = i; //olha aq
+    while text[i] != '\"' {
+        println!("{}", text[i]);
+        i += 1
+    }
+    offset = i - offset;
+    println!("{ptr} {offset}");
+    let content: String = text[ptr..offset].iter().collect();
+    attrs.push(Atributte { name, content });
+    attrs
 }
 #[derive(Debug)]
 struct Atributte {
@@ -112,8 +69,7 @@ impl VoidTag {
 fn main() {
     let mut arq: String = fs::read_to_string("test.smkl").unwrap();
     arq = arq.trim().to_string();
-    let mut vec_chars: Vec<char> = arq.chars().collect();
-    vec_chars.push('\0');
-    println!("{:?}\n\n\n\n\n\n\n", get_tag(&vec_chars));
-    println!("{arq}");
+    println!("{arq}\n\n\n");
+
+    println!("{:?}", get_attrs(&arq.chars().collect::<Vec<char>>()));
 }
